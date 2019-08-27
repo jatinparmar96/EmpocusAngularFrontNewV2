3cd3ad8e4c3ed9e648a93e0213441a06f1fc9d82
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2019-08-27 19:03:27 +0530

    Update employee-edit-resolve.service.ts

--- src/app/crm/resolvers/employee-edit-resolve.service.ts
+++ src/app/crm/resolvers/employee-edit-resolve.service.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
 import { EmployeeService } from '../services/employee/employee.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeEditResolveService implements Resolve<any> {
-
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const param = route.paramMap.get('id');
-    if (param === 'new') {
+    const param = +route.paramMap.get('id');
+    if (param === undefined || null) {
       return false;
-    }
-    else {
+    } else {
       return this._employeeSerivce.get(param);
     }
   }
 
-
-  constructor(
-    private _employeeSerivce: EmployeeService
-  ) { }
+  constructor(private _employeeSerivce: EmployeeService) {}
 }
