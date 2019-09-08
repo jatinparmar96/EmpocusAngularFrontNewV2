f37f6d1f527e3cc9561bfbb8764308e3e709d672
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2019-09-08 17:32:21 +0530

    update Employee Selector

--- src/app/crm/selector-components/employee-selector/employee-selector.component.ts
+++ src/app/crm/selector-components/employee-selector/employee-selector.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 import { Observable, Subject, concat, of } from 'rxjs';
 import {
   debounceTime,
@@ -9,14 +17,16 @@ import {
   map
 } from 'rxjs/operators';
 import { EmployeeService } from 'app/crm/services/employee/employee.service';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-employee-selector',
   templateUrl: './employee-selector.component.html',
   styleUrls: ['./employee-selector.component.scss']
 })
-export class EmployeeSelectorComponent implements OnInit {
-  @Input() parentForm: string;
+export class EmployeeSelectorComponent implements OnInit, OnChanges {
+  @Input() parentForm: FormGroup;
   @Input() controlName: string;
+  @Input() controlValue: any;
   @Output() value = new EventEmitter();
 
   contact: Observable<any>;
@@ -28,23 +38,40 @@ export class EmployeeSelectorComponent implements OnInit {
   ngOnInit() {
     this.loadItems();
   }
-  private loadItems() {
-    this.contact = concat(
-      of([]), // default items
-      this.contactInput.pipe(
-        debounceTime(200),
-        distinctUntilChanged(),
-        tap(() => (this.contactLoading = true)),
-        switchMap(term =>
-          this._employeeService.searchEmployeeName(term).pipe(
-            catchError(() => of([])), // empty list on error
-            map((val: any) => (val = val.data)),
-            tap(() => {
-              this.contactLoading = false;
-            })
+  private loadItems(value: any = '') {
+    if (value) {
+      this.contact = of([value]);
+      setTimeout(() => {
+        this.parentForm.controls[this.controlName].setValue(value);
+      }, 100);
+    } else {
+      this.contact = concat(
+        of([]), // default items
+        this.contactInput.pipe(
+          debounceTime(200),
+          distinctUntilChanged(),
+          tap(() => (this.contactLoading = true)),
+          switchMap(term =>
+            this._employeeService.searchEmployeeName(term).pipe(
+              catchError(() => of([])), // empty list on error
+              map((val: any) => (val = val.data)),
+              tap(() => {
+                this.contactLoading = false;
+              })
+            )
           )
         )
-      )
-    );
+      );
+    }
+  }
+
+  trackBy(item) {
+    return item.id;
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.controlValue && !changes.controlValue.firstChange) {
+      const value = changes.controlValue.currentValue;
+      this.loadItems(value);
+    }
   }
 }
