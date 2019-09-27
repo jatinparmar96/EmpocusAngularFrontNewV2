b0da935934f82e84ccd3b7de5bfb0d81645aec41
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2019-09-27 12:36:31 +0530

    Bank Master View

--- src/app/master/components/bank-master/bank-master-view/bank-master-view.component.ts
+++ src/app/master/components/bank-master/bank-master-view/bank-master-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'app/shared/services/api.service';
 
 @Component({
   selector: 'app-bank-master-view',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bank-master-view.component.scss']
 })
 export class BankMasterViewComponent implements OnInit {
-
-  constructor() { }
+  rows: any;
+  link: any = '/dashboard/bank-master/new';
+  paginationData: any = {
+    total: 0,
+    from: 0,
+    to: 0,
+    prev_page_url: null,
+    next_page_url: null,
+    per_page: 20,
+    current_page: 1
+  };
+  constructor(private router: Router, private apiService: ApiService) {}
 
   ngOnInit() {
+    this.getData();
   }
-
+  toCreate() {
+    this.router.navigateByUrl('/dashboard/bank-master/new');
+  }
+  edit(bank_id) {
+    this.router.navigateByUrl('/dashboard/bank-master/' + bank_id);
+  }
+  getData(page = 1) {
+    this.apiService.get('admin/bank?page=' + page).then(data => {
+      let l_data: any = data;
+      l_data = l_data.data;
+      this.rows = l_data.data;
+      this.paginationData = {
+        total: l_data.total,
+        from: l_data.from,
+        to: l_data.to,
+        prev_page_url: l_data.prev_page_url,
+        next_page_url: l_data.next_page_url,
+        per_page: l_data.per_page,
+        current_page: l_data.current_page,
+        id: 'get_list'
+      };
+    });
+  }
 }
