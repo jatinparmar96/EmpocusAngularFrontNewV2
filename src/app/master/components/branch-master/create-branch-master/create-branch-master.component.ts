99e86c251a3326bfad779ef9cf1f1ea630f866f9
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2019-09-29 14:26:25 +0530

    Fixed Branch

--- src/app/master/components/branch-master/create-branch-master/create-branch-master.component.ts
+++ src/app/master/components/branch-master/create-branch-master/create-branch-master.component.ts
@@ -27,28 +27,19 @@ export class CreateBranchMasterComponent implements OnInit {
     private notifyService: NotifyService,
     private router: Router
   ) {
-    apiService.get('admin/bank_full_list').then(data => {
-      const l_data: any = data;
-      if (l_data.status) {
-        this.data = l_data.data;
+    apiService.get('admin/bank_full_list').then((data: any) => {
+      if (data.status) {
+        this.data = data.data;
       }
     });
     this.resetErrorMessages();
     this.branch_data = fb.group({
       id: ['new', Validators.required],
-      address_id: ['new', Validators.required],
       branch_name: ['', Validators.required],
       branch_code: ['', Validators.required],
       branch_gst_number: ['', Validators.required],
-      branch_address_building: ['', Validators.required],
-      branch_address_road_name: ['', Validators.required],
-      branch_address_landmark: ['', Validators.required],
-      branch_address_pincode: ['', Validators.required],
-      branch_address_country: ['', Validators.required],
-      branch_address_state: ['', Validators.required],
-      branch_address_city: ['', Validators.required],
       branch_bank_id: ['', Validators.required],
-      branch_godown: ['No', Validators.required]
+      branch_godown: [0, Validators.required]
     });
   }
 
@@ -69,9 +60,18 @@ export class CreateBranchMasterComponent implements OnInit {
 
   getData(id: any) {
     this.apiService.get('admin/branch/' + id).then(data => {
-      const l_data: any = data;
-      this.branch_data.patchValue(l_data.data);
-      console.log(this.branch_data.value);
+      let l_data: any = data;
+      l_data = l_data.data;
+      console.log(l_data.address);
+      this.branch_data.patchValue({
+        id: l_data.id,
+        branch_name: l_data.branch_name,
+        branch_code: l_data.branch_code,
+        branch_gst_number: l_data.branch_gst_number,
+        branch_bank_id: l_data.branch_bank_id,
+        branch_godown: l_data.branch_godown,
+        address: l_data.address
+      });
     });
   }
   addOrUpdate(branch) {
@@ -103,6 +103,7 @@ export class CreateBranchMasterComponent implements OnInit {
             },
             'success'
           );
+          this.router.navigate(['/master', 'branch-master']);
         } else {
           this.notifyService.show(
             {
